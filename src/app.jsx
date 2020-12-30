09019098c71511a5a21cbb377ade3368ceb2732a
JJ <jj.vega86@gmail.com>
2020-12-30 14:02:29 -0500

    Added root component and set up index.js

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class App extends Component{
+    state = {}
+    render(){
+        return(
+            <h1>Music Library 3.0</h1>
+        );
+    }
+}
+
+export default App;
+ 
