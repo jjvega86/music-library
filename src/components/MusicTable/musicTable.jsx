6fe86af06076fcdffea5f2db59da4033c1668c2c
JJ <jj.vega86@gmail.com>
2020-12-31 10:25:49 -0500

    Implemented Axios GET request and Bootstrap table logic with map function in musicTable.jsx

--- src/components/MusicTable/musicTable.jsx
+++ src/components/MusicTable/musicTable.jsx
@@ -17,9 +17,29 @@ class MusicTable extends Component{
     }
     render(){
         return(
-            <ul>
-                {this.state.songs.map(song => <li>{song.title}</li>)}
-            </ul>
+            
+            <table className="table table-dark">
+                <thead>
+                    <tr>
+                        <th scope="col">Title</th>
+                        <th scope="col">Album</th>
+                        <th scope="col">Artist</th>
+                        <th scope="col">Genre</th>
+                        <th scope="col">Release Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {this.state.songs.map(song => 
+                <tr key={song.id}>
+                    <td>{song.title}</td>
+                    <td>{song.album}</td>
+                    <td>{song.artist}</td>
+                    <td>{song.genre}</td>
+                    <td>{song.releaseDate}</td>
+                </tr>                          
+                )}                 
+                </tbody>           
+            </table>
         )
 
     }
