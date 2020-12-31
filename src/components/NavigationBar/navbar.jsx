4183a140cf5404ac1385f4303b6a07794ca0969c
JJ <jj.vega86@gmail.com>
2020-12-31 09:47:15 -0500

    Added branding icon and finalized formatting of navbar

--- src/components/NavigationBar/navbar.jsx
+++ src/components/NavigationBar/navbar.jsx
@@ -5,7 +5,7 @@ function NavBar(props){
     return(
         <div className="row">
             <nav class="navbar navbar-dark bg-dark">
-                <span class="navbar-brand mb-0 h1">Music Library</span>
+                <div className="navbar-brand"></div>
             </nav>
         </div>
     )
