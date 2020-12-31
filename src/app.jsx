41e19eb731d47411fb3eebf45e840c6993e231b8
JJ <jj.vega86@gmail.com>
2020-12-31 11:18:38 -0500

    Shifted Axios request to App.jsx and converted MusicTable into a function component

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 import './app.css';
 import NavBar from './components/NavigationBar/navbar';
 import Banner from './components/Banner/banner';
@@ -6,7 +7,19 @@ import MusicTable from './components/MusicTable/musicTable';
 import Footer from './components/Footer/footer';
 
 class App extends Component{
-    state = {}
+    state = {
+        songs: []
+    }
+
+    componentDidMount(){
+        axios.get('http://www.devcodecampmusiclibrary.com/api/music')
+        .then(res => {
+            const songs = res.data;
+            console.log(res.status);
+            this.setState({songs});
+        })
+    }
+
     render(){
         return(
             <div className="container-fluid">
--- src/components/MusicTable/musicTable.jsx
+++ src/components/MusicTable/musicTable.jsx
@@ -1,48 +1,35 @@
-import React, {Component} from 'react';
-import axios from 'axios';
+import React from 'react';
 import './musicTable.css';
 
-class MusicTable extends Component{
-    state = {
-        songs: []
-    }
+function MusicTable (props){
+    
+    return(
+        
+        <table className="table table-light">
+            <thead>
+                <tr>
+                    <th scope="col">Title</th>
+                    <th scope="col">Album</th>
+                    <th scope="col">Artist</th>
+                    <th scope="col">Genre</th>
+                    <th scope="col">Release Date</th>
+                </tr>
+            </thead>
+            <tbody>
+            {this.props.songs.map(song => 
+            <tr key={song.id}>
+                <td>{song.title}</td>
+                <td>{song.album}</td>
+                <td>{song.artist}</td>
+                <td>{song.genre}</td>
+                <td>{song.releaseDate}</td>
+            </tr>                          
+            )}                 
+            </tbody>           
+        </table>
+    )
 
-    componentDidMount(){
-        axios.get('http://www.devcodecampmusiclibrary.com/api/music')
-        .then(res => {
-            const songs = res.data;
-            console.log(res.status);
-            this.setState({songs});
-        })
-    }
-    render(){
-        return(
-            
-            <table className="table table-light">
-                <thead>
-                    <tr>
-                        <th scope="col">Title</th>
-                        <th scope="col">Album</th>
-                        <th scope="col">Artist</th>
-                        <th scope="col">Genre</th>
-                        <th scope="col">Release Date</th>
-                    </tr>
-                </thead>
-                <tbody>
-                {this.state.songs.map(song => 
-                <tr key={song.id}>
-                    <td>{song.title}</td>
-                    <td>{song.album}</td>
-                    <td>{song.artist}</td>
-                    <td>{song.genre}</td>
-                    <td>{song.releaseDate}</td>
-                </tr>                          
-                )}                 
-                </tbody>           
-            </table>
-        )
-
-    }
+    
 }
 
 export default MusicTable;
