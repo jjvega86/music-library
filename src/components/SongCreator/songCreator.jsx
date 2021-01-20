7fe5868cc22c5bc605f9f5bcc81aa2bc6504a566
JJ <jj.vega86@gmail.com>
2021-01-19 20:23:07 -0500

    Added SongCreator class component to project

---
+++ src/components/SongCreator/songCreator.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class SongCreator extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {}
+    }
+
+    render(){
+        return(
+            <div className="text-center">
+
+            </div>
+        );
+    }
+}
